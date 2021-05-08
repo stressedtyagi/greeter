43ac2cca8517a4c4b1b8408f9b7a428471db97ed
creatorgaming <divyanshutyagi8@gmail.com>
2021-05-08 21:57:28 +0530

    Basic functionality of greeter framework done

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var myGreet = $G('Divyanshu','Tyagi','hi');
-myGreet.greet().setLang('es').greet(true);
+$('#login').click(function() {
+    // Creating new object using our greeter framework
+    var loginGrt = $G('Divyanshu','Tyagi');
+    // hide login div
+    $('#logindiv').hide();
+    // Using Chainable funtionality we added to greeter framework
+    loginGrt.setLang($('#lang').val()).HTMLGreeting('#greeting',true).log();
+})
